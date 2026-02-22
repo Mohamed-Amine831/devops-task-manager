9747290d1bdef524babc626e933e1a09b4355197
Mohamed-Amine831 <loukili.mohamedamine@etu.uae.ac.ma>
2026-02-22 20:47:47 +0000

    Update welcome message on main branch

--- src/app.js
+++ src/app.js
@@ -3,28 +3,10 @@ const app = express();
 app.use(express.json());
 
 const tasksRouter = require('./routes/tasks'); 
-
-let tasks = [
-  { id: 1, title: "Initial task", completed: true },
-  { id: 2, title: "Install Git and Node.js", "completed": true },
-  { id: 2, title: "Learn DevOps basics", completed: false }
-  
-];
-
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running..." });
+  res.json({ message: "Welcome from FEATURE branch" });
 });
 
 app.use('/tasks', tasksRouter);
-
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
-});
-
-app.post('/tasks', (req, res) => {
-  const newTask = { id: tasks.length+1, title: req.body.title, completed: false };
-  tasks.push(newTask);
-  res.status(201).json(newTask);
-});
 
 app.listen(3000, ()=> console.log("API running on port 3000"));

56672b344c07ab2b2af4b72758cf0ae7b7ddff3f
Mohamed-Amine831 <loukili.mohamedamine@etu.uae.ac.ma>
2026-03-19 18:35:22 +0000

    Add Docker build as automated delivery step

--- src/app.js
+++ src/app.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
-const tasksRouter = require('./routes/tasks'); 
-
-let tasks = [
-  { id: 1, title: "Initial task", completed: true },
-  { id: 2, title: "Install Git and Node.js", "completed": true },
-  { id: 2, title: "Learn DevOps basics", completed: false }
-  
-];
+const tasksRouter = require('./routes/tasks');
+app.use('/tasks', tasksRouter);
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.json({ message: "Welcome from MAIN branch" });
-=======
   res.json({ message: "DevOps Task Manager API is running..." });
->>>>>>> feature/routes-refactor
 });
 
-app.use('/tasks', tasksRouter);
+if (require.main === module) {
+  app.listen(3000, () => console.log("API running on port 3000"));
+}
 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
-});
-
-app.post('/tasks', (req, res) => {
-  const newTask = { id: tasks.length+1, title: req.body.title, completed: false };
-  tasks.push(newTask);
-  res.status(201).json(newTask);
-});
-
-app.listen(3000, ()=> console.log("API running on port 3000"));
-
-const tasksRouter = require('./routes/tasks'); 
-app.use('/tasks', tasksRouter);
+module.exports = app;
